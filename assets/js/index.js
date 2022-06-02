551f8904949f369c3580337c4ee279bf674af25c
Ruan <ruan_kaylo@yahoo.com.br>
2022-06-02 00:47:55 -0300

    Adding more style :ok:

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,13 +11,3 @@ function showPage(){
 // Get the current year
 var year = document.getElementById('year');
 year.innerText = new Date().getFullYear();
-
-
-// Send msg to whatsapp
-var number   = '86989249062';
-var text_one = 'Estou%20interessado!%20Gostaria%20de%20saber%20%sobre%20o%20';
-var text_two = 'Estou%20interessado!%20Qual%20a%20diferenca%20entre%20os%20servidores?'
-
-var btn_25 = document.querySelector('.btn_25')
-var btn_30 = document.querySelector('.btn_30')
-var btn_35 = document.querySelector('.btn_35')
